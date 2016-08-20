2320efefd09020369053dd60d0d41106a20331b2
NikolaiCinotti <agencianiko@hotmail.com>
2016-08-20 18:04:21 -0300

    Função simples, com retorno, com parâmetro
    def, early return, parametro

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,37 +1,64 @@
-puts "Bem vindo ao jogo da adivinhação"
-puts "\nQual é o seu nome?"
-nome = gets
+def boas_vindas
 
-puts "\n\n\nComeçaremos o jogos para você, " + nome
+	puts "Bem vindo ao jogo da adivinhação"
+	puts "\nQual é o seu nome?"
+	nome = gets
 
-puts "\nEscolhendo um número secreto entre 0 e 200..."
-numero_secreto = 175
-puts "Escolhido... que tal adivinhar hoje nosso número secreto?\n\n\n"
+	puts "\n\n\nComeçaremos o jogos para você, " + nome
+
+end
+def sorteia_num_secreto
+	
+	puts "\nEscolhendo um número secreto entre 0 e 200..."
+	sorteado = 175
+	puts "Escolhido... que tal adivinhar hoje nosso número secreto?\n\n\n"
+
+	return sorteado
+end
+
+def pede_um_numero(tentativas, limite_de_tentativas)
+	puts "\n\nTentativa " + tentativas.to_s + " de " + limite_de_tentativas.to_s
+	puts "Entre com o numero"
+	chute = gets
+	puts "Será que acertou? você chutou " + chute
+
+	return chute.to_i
+
+end
+
+def verifica_se_acertou(numero_secreto, chute)
+
+	acertou = numero_secreto == chute
+
+	if acertou
+		puts "\nAcertou!"
+		return true
+
+	end
+
+	maior = numero_secreto > chute
+	if maior
+
+		puts "\nO número secreto é maior!"		
+		else
+			puts "O número secreto é menor!"
+		end
+	end
+	 false
+	
+end
+
+boas_vindas
+numero_secreto = sorteia_num_secreto 
 
 limite_de_tentativas = 5
 
 for  tentativas in 1..limite_de_tentativas
 
-	puts "\n\nTentativa " + tentativas.to_s + "de " + limite_de_tentativas
-	puts "Entre com o numero"
-	chute = gets
-	puts "Será que acertou? você chutou " + chute
+	chute = pede_um_numero(tentativas, limite_de_tentativas)
 
-	acertou = numero_secreto == chute.to_i
-
-	if acertou
-		puts "\nAcertou!"
-
-	else
-
-		maior = numero_secreto > chute.to_i
-		if maior
-			puts "\nO número secreto é maior!"
-		
-		else
-			puts "O número secreto é menor!"
-		end
-
+	if verifica_se_acertou(numero_secreto, chute)
+		break
 	end
 
 end
