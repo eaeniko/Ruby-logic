88508649a98da4efeea1badce56fbf96fb9a6a54
NikolaiCinotti <agencianiko@hotmail.com>
2016-08-20 22:00:21 -0300

    Array

--- Testes.rb
+++ Testes.rb
@@ -9,3 +9,21 @@ for dedos in 1..3
 	puts "Contando..." + dedos.to_s
 
 end
+puts "\n\n"
+
+chutes = []
+
+chute = 176
+tentativa = 1
+chutes[tentativa -1] = chute
+
+chute = 100
+tentativa = 2
+chutes[tentativa -1] = chute
+
+puts chutes[0]
+puts chutes[1]
+
+for contador in 1..tentativa
+	puts "Chute: " + chutes[contador - 1].to_s
+end
--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -16,8 +16,9 @@ def sorteia_num_secreto
 	return sorteado
 end
 
-def pede_um_numero(tentativas, limite_de_tentativas)
+def pede_um_numero(chutes, tentativas, limite_de_tentativas)
 	puts "\n\nTentativa " + tentativas.to_s + " de " + limite_de_tentativas.to_s
+	puts "Chutes até agora: " + chutes.to_s
 	puts "Entre com o numero"
 	chute = gets
 	puts "Será que acertou? você chutou " + chute
@@ -42,20 +43,22 @@ def verifica_se_acertou(numero_secreto, chute)
 		puts "\nO número secreto é maior!"		
 		else
 			puts "O número secreto é menor!"
-		end
 	end
-	 false
-	
+	 false	
 end
 
 boas_vindas
 numero_secreto = sorteia_num_secreto 
 
 limite_de_tentativas = 5
+chutes = []
+total_de_chutes = 0
 
 for  tentativas in 1..limite_de_tentativas
 
-	chute = pede_um_numero(tentativas, limite_de_tentativas)
+	chute = pede_um_numero(chutes, tentativas, limite_de_tentativas)
+	chutes[total_de_chutes] = chute
+	total_de_chutes = total_de_chutes + 1
 
 	if verifica_se_acertou(numero_secreto, chute)
 		break
