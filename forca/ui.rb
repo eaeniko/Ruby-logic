4dad5ab0a2ff3b60321ec15125bf8ba1c8a47610
NikolaiCinotti <agencianiko@hotmail.com>
2016-08-26 12:30:33 -0300

    Isolando UI e MAIN

---
+++ forca/ui.rb
@@ -0,0 +1,56 @@
+def avisa_chute_efetuado chute
+	puts "Você já chutou #{chute}"	
+end
+
+def avisa_letra_nao_encontrada
+	puts "Letra não encontrada."	
+end
+
+def avisa_letra_encontrada(total_encontrado)
+	puts "Letra encontrada #{total_encontrado} vezes."	
+end
+
+def avisa_acertou_palavra
+	puts "Parabéns! Acertou!"	
+end
+
+def avisa_errou_palavra
+	puts "Que pena... errou"	
+end
+
+def avisa_pontos(pontos_ate_agora)
+	puts "Você ganhou #{pontos_ate_agora} pontos."	
+end
+
+def da_boas_vindas
+	puts "Bem vindo ao jogo da forca"
+	puts "Qual é o seu nome?"
+	nome = gets.strip
+	puts "\n\n\n"
+	puts "Começaremos o jogo para você #{nome}."
+	nome
+end
+
+def escolhe_palavra_secreta
+	puts "Escolhendo uma palavra secreta..."
+	palavra_secreta = "programador"
+	puts "Palavras secretas com #{palavra_secreta.size} letras .... boa sorte!"
+	palavra_secreta
+end
+
+def nao_quer_jogar?
+	puts "Deseja jogar novamente? S/N"
+	quero_jogar = gets.strip
+	nao_quer_jogar = quero_jogar.upcase == "N"
+	
+end
+
+def pede_um_chute(chutes, erros)
+	puts "\n\n\n\n"
+	puts "Erros até agoar: #{erros}"
+	puts "Chutes até agora: #{chutes}"
+	puts "Entre com uma palavra ou letra"
+	chute = gets.strip
+	puts "Será que acertou? Você chutou #{chute}"
+	chute
+end
