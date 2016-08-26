f7d3e269a7ead35cfb4824412284abb71ee592f7
NikolaiCinotti <agencianiko@hotmail.com>
2016-08-26 18:59:50 -0300

    Finalizando pilha de execução

--- forca/nomes.rb
+++ forca/nomes.rb
@@ -10,3 +10,27 @@ def pede_nome
 	puts "Pedido!"
 	nome_lido
 end
+
+def inicio
+	nome = pede_nome
+	puts "Bem vindo #{nome}"
+
+	puts "Quero conhecer mais alguém"
+	nome2 = pede_nome
+	puts "Olá #{nome2}"
+end
+
+inicio
+
+# nomes.rb:9 in pede_nome (nome_lido  "João")
+# nomes.rb:17 in inicio (nome = "Nikolai")
+# nomes.rb:21 in main
+
+# Digite seu nome
+# Lido Nikolai
+# Pedido!
+# Bem vindo Nikolai
+# Quero conhecer mais alguém
+# Digite seu nome
+# Lido João
+# Olá João
