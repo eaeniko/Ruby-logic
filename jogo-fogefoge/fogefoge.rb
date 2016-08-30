b8043cf1f7e34dce4d58b26127797b5044838507
NikolaiCinotti <agencianiko@hotmail.com>
2016-08-30 11:37:02 -0300

    evitando fantasma passar da barreira

--- jogo-fogefoge/fogefoge.rb
+++ jogo-fogefoge/fogefoge.rb
@@ -50,10 +50,11 @@ def posicao_valida?(mapa, posicao)
 end
 
 def move_fantasma(mapa, linha, coluna)
-	mapa[linha] [coluna] = " "
-	linha += 0
-	coluna += 1
-	mapa[linha] [coluna] = "F"
+	posicao = [linha, coluna +1]
+	if posicao_valida? mapa, posicao
+		mapa[linha] [coluna] = " "
+		mapa[posicao[0]] [posicao[1]] = "F"	
+	end
 	
 end
 
