07a6b82296ff9e5125d6ba095e9f7dd5853c9cb6
NikolaiCinotti <agencianiko@hotmail.com>
2016-08-29 23:15:23 -0300

    Arra Associativo

--- jogo-fogefoge/fogefoge.rb
+++ jogo-fogefoge/fogefoge.rb
@@ -20,16 +20,15 @@ end
 
 def calcula_nova_posicao(heroi, direcao)
 	heroi = heroi.dup
-	case direcao
-		when "W"
-			heroi[0] -= 1
-		when "S"
-			heroi[0] += 1
-		when "A"
-			heroi[1] -= 1
-		when "D"
-			heroi[1] += 1
-	end
+	movimentos = {
+		"W" => [-1, 0],
+		"S" => [+1, 0],
+		"A" => [0, -1],
+		"D" => [0, +1]
+	}
+	movimentos = movimentos[direcao]
+	heroi[0] += movimentos[0]
+	heroi[1] += movimentos[1]
 	heroi
 end
 
