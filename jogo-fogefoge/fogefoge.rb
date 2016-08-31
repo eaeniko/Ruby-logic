a12821d6f844ccc393ecf32714a8e8cab5d739fa
NikolaiCinotti <agencianiko@hotmail.com>
2016-08-30 23:50:35 -0300

    movimentando fantasma no novo mapa

--- jogo-fogefoge/fogefoge.rb
+++ jogo-fogefoge/fogefoge.rb
@@ -49,35 +49,35 @@ def posicao_valida?(mapa, posicao)
 	true
 end
 
-def posicoes_validas_a_partir_de(mapa, posicao)
+def posicoes_validas_a_partir_de(mapa, novo_mapa, posicao)
 	posicoes = []
 	baixo = [posicao[0] +1, posicao[1]]
-	if posicao_valida? mapa, baixo
+	if posicao_valida?(mapa, baixo) && posicao_valida?(novo_mapa, baixo)
 		posicoes << baixo
 	end
 	direita = [posicao[0], posicao[1] + 1]
-	if posicao_valida? mapa, baixo
+	if posicao_valida?(mapa, direita) && posicao_valida?(novo_mapa, direita)
 		posicoes << direita
 	end
 	cima = [posicao[0] -1, posicao[1]]
-	if posicao_valida? mapa, baixo
+	if posicao_valida?(mapa, cima) && posicao_valida?(novo_mapa, cima)
 		posicoes << cima
 	end
 	esquerda = [posicao[0], posicao[1] -1]
-	if posicao_valida? mapa, baixo
+	if posicao_valida?(mapa, esquerda) && posicao_valida?(novo_mapa, esquerda)
 		posicoes << esquerda
 	end
 	posicoes	
 end
 
-def move_fantasma(mapa, linha, coluna)
-	posicoes = posicoes_validas_a_partir_de mapa, [linha, coluna]
+def move_fantasma(mapa, novo_mapa, linha, coluna)
+	posicoes = posicoes_validas_a_partir_de mapa, novo_mapa, [linha, coluna]
 	
 	return if posicoes.empty?
 
 	posicao = posicoes[0]
 	mapa[linha] [coluna] = " "
-	mapa[posicao[0]] [posicao[1]] = "F"		
+	novo_mapa[posicao[0]] [posicao[1]] = "F"		
 end
 
 def copia_mapa(mapa)
@@ -86,14 +86,16 @@ end
 
 def move_fantasmas(mapa)
 	caractere_fantasma = "F"
+	novo_mapa = copia_mapa mapa
 	mapa.each_with_index do |linha_atual, linha|
 		linha_atual.chars.each_with_index do |caractere_atual, coluna|
 			eh_fantasma = caractere_atual == caractere_fantasma
 			if eh_fantasma
-				move_fantasma mapa, linha, coluna
+				move_fantasma mapa, novo_mapa, linha, coluna
 			end
 		end
-	end	
+	end
+	novo_mapa	
 end
 
 def joga(nome)
@@ -111,7 +113,7 @@ def joga(nome)
 		mapa [heroi[0]] [heroi[1]] = " "
 		mapa [nova_posicao[0]] [nova_posicao[1]] = "H"
 
-		move_fantasmas mapa						
+		mapa = move_fantasmas mapa						
 	end	
 end
 
