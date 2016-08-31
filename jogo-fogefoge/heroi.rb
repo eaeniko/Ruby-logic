85a8f875d95a4aa01b896e829d0fcf0c259aa86e
NikolaiCinotti <agencianiko@hotmail.com>
2016-08-31 15:39:52 -0300

    code smell : feature envy

--- jogo-fogefoge/heroi.rb
+++ jogo-fogefoge/heroi.rb
@@ -1,4 +1,18 @@
 class Heroi
 	attr_accessor :linha, :coluna
 
+
+	def calcula_nova_posicao(direcao)
+		novo_heroi = dup
+		movimentos = {
+			"W" => [-1, 0],
+			"S" => [+1, 0],
+			"A" => [0, -1],
+			"D" => [0, +1]
+		}
+		movimentos = movimentos[direcao]
+		novo_heroi.linha += movimentos[0]
+		novo_heroi.coluna += movimentos[1]
+		novo_heroi
+	end
 end
